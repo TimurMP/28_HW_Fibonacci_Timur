dc3946914083a297c5e494ca63778b1b4b0ae060
TimurMP <titimurmamyshev@gmail.commur>
2022-07-07 22:34:20 +0300

    Baseline

--- src/telran/fibonacci/controller/FibonacciAppl.java
+++ src/telran/fibonacci/controller/FibonacciAppl.java
@@ -1,2 +1,17 @@
-package telran.fibonacci.controller;public class FibonacciAppl {
+package telran.fibonacci.controller;
+
+import telran.fibonacci.model.Fibonacci;
+
+public class FibonacciAppl {
+    public static void main(String[] args) {
+        Fibonacci fib = new Fibonacci(8);
+        for (Integer num: fib){
+            //TODO: print all numbers 1,1,2,3,5,8,13,21
+
+
+        }
+        //TODO: print sum of all numbers 1,1,2,3,5,8,13,21 (54)
+
+    }
+
 }
--- src/telran/fibonacci/model/Fibonacci.java
+++ src/telran/fibonacci/model/Fibonacci.java
@@ -1,2 +1,36 @@
-package telran.fibonacci.model;public class Fibonacci {
+package telran.fibonacci.model;
+
+import java.util.Iterator;
+
+public class Fibonacci implements Iterable <Integer> {
+    private int quantity;
+
+    public Fibonacci(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<Integer>() {
+
+            @Override
+            public boolean hasNext() {
+                return false;
+            }
+
+            @Override
+            public Integer next() {
+                return null;
+            }
+        };
+    }
 }
