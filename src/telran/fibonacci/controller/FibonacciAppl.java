c07c3466d511095089678c0f47ed795ab977ba63
TimurMP <titimurmamyshev@gmail.commur>
2022-07-08 03:40:26 +0300

    Final 1.0

--- src/telran/fibonacci/controller/FibonacciAppl.java
+++ src/telran/fibonacci/controller/FibonacciAppl.java
@@ -6,10 +6,9 @@ public class FibonacciAppl {
     public static void main(String[] args) {
         Fibonacci fib = new Fibonacci(8);
         int sum = 0;
-        for (Integer num: fib){
-
+        for (Integer num : fib) {
             System.out.print(num + "\t");
-            sum+=num;
+            sum += num;
         }
         System.out.println("\n" + sum);
 
